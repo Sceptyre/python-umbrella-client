ce95a08745e8e3699801bb024a95c3d0d0107aa0
Abdellah <abdellah.fytahi@karglobal.com>
2023-01-11 16:35:52 -0500

    linting

--- umbrellaclient/api/destinationlists.py
+++ umbrellaclient/api/destinationlists.py
@@ -9,7 +9,7 @@ class DestinationListAPI(_BaseAPI):
         res.raise_for_status()
 
         return res.json()
-    
+
     def get_destination_lists(self):
         res = self._uc.s.get(
             'https://api.umbrella.com/policies/v2/destinationlists'
@@ -20,7 +20,13 @@ class DestinationListAPI(_BaseAPI):
 
 
     # Destination interactions
-    def add_destination_list_destination(self, destination_list_id: str, destination: str, type: str='DOMAIN', comment: str='Added By Automation'):
+    def add_destination_list_destination(
+        self, 
+        destination_list_id: str, 
+        destination: str, 
+        type: str='DOMAIN', 
+        comment: str='Added By Automation'
+    ):
         res = self._uc.s.post(
             'https://api.umbrella.com/policies/v2/destinationlists/' + destination_list_id + '/destinations',
             json=[
@@ -34,7 +40,12 @@ class DestinationListAPI(_BaseAPI):
 
         return res.json()
 
-    def get_destination_list_destinations(self, destination_list_id: str, page: int=1, limit: int=100):
+    def get_destination_list_destinations(
+        self, 
+        destination_list_id: str, 
+        page: int=1, 
+        limit: int=100
+    ):
         res = self._uc.s.get(
             'https://api.umbrella.com/policies/v2/destinationlists/' + destination_list_id + '/destinations',
             params={
--- umbrellaclient/client.py
+++ umbrellaclient/client.py
@@ -8,14 +8,14 @@ class UmbrellaClient():
     destination_lists: api.DestinationListAPI
 
     def _get_token(self):
-        r = self.s.post(
+        res = self.s.post(
             'https://api.umbrella.com/auth/v2/token',
             data={
                 'grant_type':'client_credentials'
             }
         )
-        r.raise_for_status()
-        return r.json()['access_token']
+        res.raise_for_status()
+        return res.json()['access_token']
 
     def __init__(self, api_key: str, api_secret: str):
         self.s.auth = HTTPBasicAuth(api_key, api_secret)
